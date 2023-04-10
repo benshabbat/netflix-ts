aae7c52393bebf324ca1d6cd29e5f7139997829a
benshabbat <benshabbat27@gmail.com>
2023-04-11 00:05:00 +0300

    create component input and auth page ui

--- netflix/pages/auth.tsx
+++ netflix/pages/auth.tsx
@@ -1,7 +1,21 @@
+import Input from "@/components/Input";
+
 const Auth = () => {
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div></div>
+      <div className="bg-black h-full w-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.jpg" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In </h2>
+            <div className="flex flex-col gap-4 ">
+              <Input/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

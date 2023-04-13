f3230ce23976dac9ebbe0e0507db65bc813b1ee8
benshabbat <benshabbat27@gmail.com>
2023-04-13 10:25:22 +0300

    import compare

--- netflix/pages/api/[...nextAuth].ts
+++ netflix/pages/api/[...nextAuth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/lib/prismadb";
+import { compare } from "bcrypt";
 export default NextAuth({
   providers: [
     Credentials({
@@ -14,29 +15,34 @@ export default NextAuth({
         password: {
           label: "Password",
           type: "password",
-        }
+        },
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error('Email and password required');
+          throw new Error("Email and password required");
         }
 
-        const user = await prismadb.user.findUnique({ where: {
-          email: credentials.email
-        }});
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
         if (!user || !user.hashedPassword) {
-          throw new Error('Email does not exist');
+          throw new Error("Email does not exist");
         }
 
-        const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
 
         if (!isCorrectPassword) {
-          throw new Error('Incorrect password');
+          throw new Error("Incorrect password");
         }
 
         return user;
-      }
+      },
     }),
   ],
 });

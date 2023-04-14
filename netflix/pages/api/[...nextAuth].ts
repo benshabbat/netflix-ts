3d440f65fbafdb2ff5ff08f4136342caad2b062e
benshabbat <benshabbat27@gmail.com>
2023-04-14 14:11:56 +0300

    ADD jwt and secret

--- netflix/pages/api/[...nextAuth].ts
+++ netflix/pages/api/[...nextAuth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/lib/prismadb";
-import { compare } from 'bcrypt';
+import { compare } from "bcrypt";
 export default NextAuth({
   providers: [
     Credentials({
@@ -45,4 +45,15 @@ export default NextAuth({
       },
     }),
   ],
+  pages:{
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session:{
+    strategy:'jwt',
+  },
+  jwt:{
+    secret:process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret:process.env.NEXTAUTH_SECRET,
 });

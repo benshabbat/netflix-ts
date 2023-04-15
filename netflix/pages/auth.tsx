4237f52d442f505098d705fbcde404ebc1714a29
benshabbat <benshabbat27@gmail.com>
2023-04-15 23:24:14 +0300

    add login google

--- netflix/pages/auth.tsx
+++ netflix/pages/auth.tsx
@@ -88,10 +88,16 @@ const Auth = () => {
               {variant === "login" ? "Sign In" : "Register"}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
                 <FcGoogle size={20} />
               </div>
-              <div  onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
                 <BsGithub size={20} />
               </div>
             </div>

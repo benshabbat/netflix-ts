6a0d2b19cd0a8e2c9b08c33a8cfcf7dcd24a2354
benshabbat <benshabbat27@gmail.com>
2023-04-15 22:52:57 +0300

    add github and google provider and prisma adapter to next auth

--- netflix/pages/api/auth/[...nextauth].ts
+++ netflix/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,19 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismaDB from "@/lib/prismaDB";
 import { compare } from "bcrypt";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
@@ -45,15 +56,16 @@ export default NextAuth({
       },
     }),
   ],
-  pages:{
+  pages: {
     signIn: "/auth",
   },
-  debug: process.env.NODE_ENV === 'development',
-  session:{
-    strategy:'jwt',
+  debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismaDB),
+  session: {
+    strategy: "jwt",
   },
-  jwt:{
-    secret:process.env.NEXTAUTH_JWT_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
   },
-  secret:process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 });

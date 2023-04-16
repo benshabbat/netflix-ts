716bdb3a9c8b5f7e57fe93ba08fa666d2d3fdfdf
benshabbat <benshabbat27@gmail.com>
2023-04-16 10:49:17 +0300

    syntax fetcher and add data current user

--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next";
 import {getSession, signOut } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -17,9 +18,10 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 export default function Home() {
+  const {data}=useCurrentUser()
   return (
     <>
-    <h1 className="text-4xl text-blue-600">Hello</h1>
+    <h1 className="text-4xl text-blue-600">Hello {data?.name}</h1>
       <button className="h-10 w-full bg-red-600" onClick={() => signOut()}>
         LogOut
       </button>

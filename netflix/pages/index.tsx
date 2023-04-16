374a6b18801a39159c773cc60c73c997abab319b
benshabbat <benshabbat27@gmail.com>
2023-04-16 10:30:19 +0300

    create logout and protected

--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,3 +1,28 @@
+import { NextPageContext } from "next";
+import {getSession, signOut } from "next-auth/react";
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 export default function Home() {
-  return <></>;
+  return (
+    <>
+    <h1 className="text-4xl text-blue-600">Hello</h1>
+      <button className="h-10 w-full bg-red-600" onClick={() => signOut()}>
+        LogOut
+      </button>
+    </>
+  );
 }

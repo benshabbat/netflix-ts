6ebbfb89b89fab5488dbb5553bced3f2fee7a9d8
benshabbat <benshabbat27@gmail.com>
2023-04-18 16:21:45 +0300

    add bell search and img for account

--- netflix/components/Navbar.tsx
+++ netflix/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { signOut } from "next-auth/react";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import NavbarItem from "./NavbarItem";
 import MobileMenu from "./MobileMenu";
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineBell } from "react-icons/ai";
 const Navbar = () => {
   const { data } = useCurrentUser();
   const [showMobile, setShowMobile] = useState(false);
@@ -32,7 +33,23 @@ const Navbar = () => {
           className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
         >
           <p className="text-white text-sm">Browse</p>
+          <AiOutlineMenu className="text-white transition" />
           <MobileMenu visible={showMobile} />
+        </div>
+        <div className="text-white flex flex-row ml-auto gap-7 items-center">
+          <div className="flex flex-row gap-1 items-center hover:text-gray-400 cursor-pointer">
+            <AiOutlineSearch />
+            Search
+          </div>
+          <div className=" hover:text-gray-400 cursor-pointer">
+            <AiOutlineBell />
+          </div>
+          <div className="flex flex-row gap-2 items-center relative cursor-pointer">
+            <div className="w-6 h-6 lg:h-10 lg:w-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="blueProfile" />
+            </div>
+            <AiOutlineMenu className="text-white transition" />
+          </div>
         </div>
       </div>
     </nav>

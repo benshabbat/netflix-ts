e7a1ba2d455d390b54cd86d6dafbfdd4221c014d
benshabbat <benshabbat27@gmail.com>
2023-04-20 11:45:01 +0300

    create random for movie

---
+++ netflix/pages/api/random.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismaDB from "@/lib/prismaDB";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== "GET") {
+      return res.status(405).end();
+    }
+
+    await serverAuth(req, res);
+
+    const moviesCount = await prismaDB.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
+
+    const randomMovies = await prismaDB.movie.findMany({
+      take: 1,
+      skip: randomIndex,
+    });
+
+    return res.status(200).json(randomMovies[0]);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).end();
+  }
+}

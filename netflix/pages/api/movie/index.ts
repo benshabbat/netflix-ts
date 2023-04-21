f9240ad2ac58788d0784909feef231d58dbe1c98
benshabbat <benshabbat27@gmail.com>
2023-04-21 14:17:34 +0300

    create req get movies

---
+++ netflix/pages/api/movie/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismaDB from "@/lib/prismaDB";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    if (req.method !== "GET") {
+      return res.status(405).end();
+    }
+
+    await serverAuth(req, res);
+
+    const movies = await prismaDB.movie.findMany();
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
+  }
+}

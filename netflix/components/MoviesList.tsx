4d70a932cdfa0724823671033a14fc4900328f6e
benshabbat <benshabbat27@gmail.com>
2023-04-23 01:25:37 +0300

    created MoviesList

--- netflix/components/MoviesList.tsx
+++ netflix/components/MoviesList.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import {isEmpty} from "lodash"
-const MoviesList = () => {
-  return (
-    <div>MoviesList</div>
-  )
-}
+import React from "react";
+import { isEmpty } from "lodash";
+import useMoviesList from "@/hooks/useMoviesList";
 
-export default MoviesList
+// interface MoviesListProps {
+//   data: Record<string, any>[];
+//   title: string;
+// }
+const MoviesList = () => {
+  const { data:movies=[] } = useMoviesList()
+  if (isEmpty(movies)) {
+    return null;
+  }
+  return (
+    <div className="pb-40">
+      <div className="px-4 md:px-12 mt-4 space-y-8">
+        <div>
+          <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+            {"Tora"}
+          </p>
+          <div className="grid grid-col-4 gap-2">
+            {movies.map((movie) => {
+              <div key={movie.id}>{movie}</div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MoviesList;
--- netflix/pages/index.tsx
+++ netflix/pages/index.tsx
@@ -1,9 +1,9 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
-import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MoviesList from "@/components/MoviesList";
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,14 +22,12 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 export default function Home() {
-  const { data } = useCurrentUser();
+  
   return (
     <>
       <Navbar />
       <Billboard />
-      <div className="pb-40">
-        <MoviesList />
-      </div>
+      <MoviesList/>
     </>
   );
 }

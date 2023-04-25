f8551b5d48b097d0c0f458f061c90766642e59f7
benshabbat <benshabbat27@gmail.com>
2023-04-25 11:51:06 +0300

    ui fav button

--- toras/components/FavoriteButton.tsx
+++ toras/components/FavoriteButton.tsx
@@ -1,16 +1,22 @@
-import React,{useCallback,useMemo} from 'react'
-import axios from "axios"
+import React, { useCallback, useMemo } from "react";
+import axios from "axios";
 
-import useCurrentUser from '@/hooks/useCurrentUser'
-import useFavorite from '@/hooks/useFavorite'
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useFavorite from "@/hooks/useFavorite";
 
-interface FavoriteButtonProps{
-    movieId: string
+import { AiOutlinePlus } from "react-icons/ai";
+interface FavoriteButtonProps {
+  movieId: string;
 }
-const FavoriteButton:React.Fc<FavoriteButtonProps>= ({movieId}) => {
+const FavoriteButton: React.Fc<FavoriteButtonProps> = ({ movieId }) => {
   return (
-    <div>FavoriteButton</div>
-  )
-}
+    <div
+      onClick={() => {}}
+      className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+    >
+      <AiOutlinePlus className="text-white w-4 lg:w-6" />
+    </div>
+  );
+};
 
-export default FavoriteButton
+export default FavoriteButton;

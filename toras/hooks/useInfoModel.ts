a94747660b4419b935544553b4bf64969f04f08c
benshabbat <benshabbat27@gmail.com>
2023-05-01 12:40:41 +0300

    create hook use info model

--- toras/hooks/useInfoModel.ts
+++ toras/hooks/useInfoModel.ts
@@ -1,1 +1,16 @@
-import {create} from "zustand"
+import { create } from "zustand";
+export interface ModelStoreInterface {
+  movieId?: string;
+  isOpen: boolean;
+  openModel: (movieId: string) => void;
+  closeModel: () => void;
+}
+
+const useInfoModel = create<ModelStoreInterface>((set) => ({
+  movieId: undefined,
+  isOpen: false,
+  openModel: (movieId: string) => set({ isOpen: true, movieId }),
+  closeModel: () => set({ movieId: undefined, isOpen: false }),
+}));
+
+export default useInfoModel;

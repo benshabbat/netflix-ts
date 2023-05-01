e3d9a9103ef3d3dea483404854faee0a29be0667
benshabbat <benshabbat27@gmail.com>
2023-05-01 12:16:01 +0300

    add zustand

--- toras/pages/api/unfavorite.ts
+++ toras/pages/api/unfavorite.ts
@@ -1,19 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prismaDB from '@/lib/prismaDB';
+import prismaDB from "@/lib/prismaDB";
 
 import { getSession } from "next-auth/react";
 import { without } from "lodash";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    if (req.method !== 'POST') {
+    if (req.method !== "POST") {
       return res.status(405).end();
     }
 
     const session = await getSession({ req });
 
     if (!session?.user?.email) {
-      throw new Error('Not signed in');
+      throw new Error("Not signed in");
     }
 
     const { movieId } = req.body;
@@ -21,11 +24,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const existingMovie = await prismaDB.movie.findUnique({
       where: {
         id: movieId,
-      }
+      },
     });
 
     if (!existingMovie) {
-      throw new Error('Invalid ID');
+      throw new Error("Invalid ID");
     }
 
     const user = await prismaDB.user.findUnique({
@@ -35,7 +38,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     if (!user) {
-      throw new Error('Invalid email');
+      throw new Error("Invalid email");
     }
 
     const updatedFavoriteIds = without(user.favoriteIds, movieId);
@@ -46,7 +49,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
       data: {
         favoriteIds: updatedFavoriteIds,
-      }
+      },
     });
 
     return res.status(200).json(updatedUser);
